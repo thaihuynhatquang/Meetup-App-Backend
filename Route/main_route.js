c0ed9f41d2991be423abe1c39445f7cdd8c57028
linhtq <tranquanglinh.pt@gmail.com>
2019-07-25 23:33:02 +0700

    xoa code thua

--- Route/main_route.js
+++ Route/main_route.js
@@ -5,17 +5,8 @@ module.exports = {
   route: function(app) {
     app.get("/", (req, res) => res.send("Hello, I am OK now!"));
     app.post("/user/auth", jsonParser, (req, res) => user_route.auth(req, res));
-    app.post("/user/login", jsonParser, (req, res) =>
-      user_route.login(req, res)
-    );
-    app.post("/user/register", jsonParser, (req, res) =>
-      user_route.register(req, res)
-    );
     app.post("/user/loginWithGoogle", jsonParser, (req, res) =>
       user_route.loginGoogle(req, res)
-    );
-    app.get("/user/info/userID=:userID", (req, res) =>
-      user_route.sendUserInfo(req, res)
     );
   }
 };
