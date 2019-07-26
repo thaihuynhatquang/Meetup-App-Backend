e33881fd1cc6b7ac5792742282f2f4973e9d32e1
linhtq <tranquanglinh.pt@gmail.com>
2019-07-27 00:53:50 +0700

    fix loi vat o main_route

--- Route/main_route.js
+++ Route/main_route.js
@@ -12,14 +12,17 @@ module.exports = {
     app.post('/user/loginWithGoogle', jsonParser, (req, res) => user_route.loginGoogle(req, res));
     //To Do
     app.post('/user/FreeTimeForGroup', jsonParser, (req, res) => user_route.setFreeTimeForGroup(req, res));
-    app.patch('/user/profile', jsonParser, (req, res) => user_route.updateProfile(req, res)); // lam duoc thi tot
+    app.put('/user/profile', jsonParser, (req, res) => user_route.updateProfile(req, res)); // lam duoc thi tot
     app.get('/user/profile', (req, res) => user_route.getProfile(req, res)); // lam duoc thi tot
     app.get('/user/notify', (req, res) => user_route.getNotify(req, res)); // lam duoc thi tot
 
     app.post('/group/createGroup', jsonParser, (req, res) => groupadmin_route.createGroup(req, res)); //DONE
-    app.patch('/group/updateGroupInfo', jsonParser, (req, res) => groupadmin_route.updateGroupInfo(req, res)); //update cac thong tin group
-    app.patch('/group/updateGroupMembers', jsonParser, (req, res) => groupadmin_route.updateGroupMembers(req, res)); // them, sua, xoa member sau do gui lai danh sach member vao api nay
+    app.put('/group/updateGroupInfo', jsonParser, (req, res) => groupadmin_route.updateGroupInfo(req, res)); //update cac thong tin group
+    app.put('/group/updateGroupMembers', jsonParser, (req, res) => groupadmin_route.updateGroupMembers(req, res)); // them, sua, xoa member sau do gui lai danh sach member vao api nay
     app.get('/group/members_time', (req, res) => groupadmin_route.getMembersTime(req, res)); //lay du lieu thong ke de show graph
     app.post('/group/createMeeting', jsonParser, (req, res) => groupadmin_route.createMeeting(req, res)); // tao meeting moi ( sau khi da xem thong ke)
   },
 };
+
+
+
