1edddca78aacbbb7726d2cc6dc06b4bf02ad1701
NoCtrlZ <huynd.nguyen@gmail.com>
2019-07-27 11:57:36 +0700

    nghichngu

--- Model/database.js
+++ Model/database.js
@@ -23,6 +23,12 @@ var db_model = {
   },
 
   //Users
+  addUserDefaultProperties: (user)=>{
+    user.freetimes=[];
+    user.groups=[];
+    user.meetings=[];
+    return user;
+  },
   manageUser: async (newUser) => {
     try {
       let userRef = await db
@@ -30,6 +36,7 @@ var db_model = {
         .doc(newUser.userName)
         .get();
       if (!userRef.exists) {
+        newUser=this.addUserDefaultProperties(newUser);
         await db
           .collection('users')
           .doc(newUser.userName)
@@ -54,11 +61,89 @@ var db_model = {
       throw error;
     }
   },
+  getCurrentUser: async (userName) => {
+    try {
+      let user = await db
+        .collection('users')
+        .doc(userName)
+        .get();
+      return user.data();
+    } catch (error) {
+      throw error;
+    }
+  },
+  getGroupsByUserID: async (userID) => {
+    let userDoc = await db
+      .collection('users')
+      .doc(userID)
+      .get();
+    if (!userDoc.empty) {
+      // return Promise.resolve(userDoc.data().groups);
+      let gidList = userDoc.data().groups;
+      let resultArr = [];
+      for (let i = 0; i < gidList.length; i++) {
+        let gid = gidList[i];
+        let grDoc = await db
+          .collection('groups')
+          .doc(gid)
+          .get();
+        if (!grDoc.empty) resultArr.push(grDoc.data());
+      }
+      return Promise.resolve(resultArr);
+    }
+    return Promise.reject(null);
+  },
+  updateFreeTime: async (userid, listFreeTime) => {
+    let userCollection = db.collection('users');
+    let userDoc = await userCollection.doc(userid).get();
+    if (!userDoc.empty) {
+      await userCollection.doc(userid).update({ freetimes: listFreeTime });
+      return Promise.resolve(true);
+    }
+    return Promise.reject(false);
+  },
+  updateProfile: async (userid,updateObject) => {
+    let userCollection = db.collection('users');
+    let userDoc = await userCollection.doc(userid).get();
+    if (!userDoc.empty) {
+      await userCollection.doc(userid).update(updateObject);
+      return Promise.resolve(true);
+    }
+    return Promise.reject(false);
+  },
+  getUserProfile: async(uid)=>{
+    let userCollection = db.collection('users');
+    let userDoc = await userCollection.doc(userid).get();
+    if (!userDoc.empty) {
+      let user= userDoc.data();
+      delete user.dark;
+      user.id = u.id;
+      return Promise.resolve(user);
+    }
+    return Promise.reject(null);
+  },
+
 
   //Groups
+  addGroupDefaultProperties: (group)=>{
+    group.member=[];
+  },
+  getGroupInfo: async (groupID)=>{
+    let grCollection = await db
+      .collection('groups')
+      .doc(groupID)
+      .get();
+    if (!grCollection.empty) {
+      let gr = grCollection.data();
+      // gr.id=groupID;
+      return Promise.reject(gr);
+    }
+    return Promise.reject(null);
+  },
   addGroup: async (newGroup) => {
     try {
       let collection = db.collection('groups');
+      newGroup= this.addGroupDefaultProperties(newGroup);
       let currentUser = db
         .collection('users')
         .doc(newGroup.adminEmail)
@@ -71,9 +156,117 @@ var db_model = {
       return Promise.reject(error);
     }
   },
-};
+  addMemberToGroup: async (groupid, listUserID) => {
+    if (!(listUserID instanceof Array)) return Promise.reject(false);
+    let grCollection = db.collection('groups').doc(groupid);
+    let gr = await grCollection.get();
+    if (!gr.empty) {
+      let currListMember = gr.data().member;
+      // console.log(currListMember);
+      // let currListMember
+      let userCollection = db.collection('users');
+      for (let i = 0; i < listUserID.length; i++) {
+        let uid = listUserID[i];
+        currListMember.push(uid);
+        var udoc = await userCollection.doc(uid).get();
+        let newListGroup = udoc.data().groups;
+        if (newListGroup == undefined) newListGroup = [];
+        newListGroup.push(groupid);
+        await userCollection.doc(uid).update({ groups: newListGroup });
+      }
+      await grCollection.update({
+        member: currListMember,
+      });
+      return Promise.resolve(true);
+    }
+    return Promise.resolve(false);
+  },
+  searchUser: async (keysearch) => {
+    var keysearch = String(keysearch)
+      .toLowerCase()
+      .trim();
+    if (keysearch == '') return Promise.resolve(false);
+    let userCollection = db.collection('users');
+    let resultArray = await userCollection.where('userName', '==', keysearch).get();
+    if (!resultArray.empty) {
+      let userArr = [];
+      resultArray.forEach((u) => {
+        let user = u.data();
+        delete user.groups;
+        delete user.dark;
+        user.id = u.id;
+        userArr.push(user);
+      });
+      return Promise.resolve(userArr);
+    }
+    return Promise.reject(null);
+  },
+  getUserTimeInGroup: async (groupid) => {
+    let grCollection = await db
+      .collection('groups')
+      .doc(groupid)
+      .get();
+    if (!grCollection.empty) {
+      //Todo: chỉnh tên biến range_from, range_to cho hợp với tên biến trên database, 2 biến này thể hiện khoảng thời gian admin mong muốn tổ chức meetings.
+      let range_start = new Date(grCollection.data().range_from.toDate());
+      let range_end = new Date(grCollection.data().range_to.toDate());
+      let listMembers = grCollection.data().member;
+      let resultUserTimeArr = [];
+      for (var m = 0; m < listMembers.length; m++) {
+        let memid = listMembers[m];
+        let memDoc = await db
+          .collection('users')
+          .doc(memid)
+          .get();
+        if (!memDoc.empty) {
+          console.log('tìm thấy  user trong group');
+          let userTimeArr = {};
+          userTimeArr.name = memDoc.data().name;
 
+          if (memDoc.data().freetimes != undefined && memDoc.data().freetimes instanceof Array) {
+            let freeTimeList = memDoc.data().freetimes;
+            userTimeArr.freetimes = [];
+            let overDateData = [];
+            for (let i = 0; i < freeTimeList.length; i++) {
+              //check time is current or future , if not delete it
+              var freeFrom = new Date(freeTimeList[i].from.toDate());
+              var freeTo = new Date(freeTimeList[i].to.toDate());
+              var curDate = new Date();
+              if (freeTo < curDate) {
+                overDateData.push(i);
+                continue;
+              } else {
+                // if(freeFrom<=range_end){
+                let freetime = {};
+                freetime.from = freeFrom > range_start ? freeFrom : range_start;
+                freetime.to = freeTo < range_end ? freeTo : range_end;
+                userTimeArr.freetimes.push(freetime);
+                // }
+              }
+            }
+            // Todo : xóa bỏ các khoảng thời gian đã hết hạn của người dùng ( có index được lưu trong mảng overDateData)
+          }
+          resultUserTimeArr.push(userTimeArr);
+        }
+      }
+
+      return Promise.resolve(resultUserTimeArr);
+    }
+    return Promise.reject(null);
+  },
+};
+// var freetime=[{from:new Date(2019,7,27,0,0,0,0),to:new Date(2019,7,27,10,0,0,0,0)},{from:new Date(2019,8,27,0,0,0,0),to:new Date(2019,8,27,10,0,0,0,0)}]
 module.exports = db_model;
+// db_model
+//   .updateFreeTime("laivansam1998@gmail.com",freetime)
+//   .then((r) => console.log(r))
+//   .catch((e) => console.log(e));
+// db_model
+//   .getGroupInfo('SGUET.thaihuynhatquang@gmail.com')
+//   .then((r) => console.log(r))
+//   .catch((e) => console.log(e));
+// db_model.searchUser("thaihuynhatquang@gmail.com").then(r=>console.log(r)).catch(e=>console.log(e));
+// db_model.addMemberToGroup("Group1.thaihuynhatquang@gmail.com", ["00n1lBtebVkhUY5iSZPS","OJXKyv9giT6dXYTtw3zn"]);
 // var x = {
 //     username: "linhhtq@gmail.com",
 //     locations: "hihihhih",
--- Route/main_route.js
+++ Route/main_route.js
@@ -6,20 +6,20 @@ var midPoint_route = require('./midPoint_route');
 module.exports = {
   route: function(app) {
     app.get('/', (req, res) => res.send('Hello, I am OK now!'));
-    app.post('/user/auth', jsonParser, (req, res) => user_route.auth(req, res));
     app.get('/user', (req, res) => user_route.getUsers(req, res));
-
-    // To Do: trong ham loginGoole, neu user chua ton tai thi phai set them cac thuoc tinh mac dinh cua user nhu location, groups[], meetings[] (chua lam)
+    app.get('/user/auth', jsonParser, (req, res) => user_route.auth(req, res));
     app.post('/user/loginWithGoogle', jsonParser, (req, res) => user_route.loginGoogle(req, res));
+
     //To Do
     app.post('/user/FreeTimeForGroup', jsonParser, (req, res) => user_route.setFreeTimeForGroup(req, res));
-    app.patch('/user/profile', jsonParser, (req, res) => user_route.updateProfile(req, res)); // lam duoc thi tot
+    app.put('/user/profile', jsonParser, (req, res) => user_route.updateProfile(req, res)); // lam duoc thi tot
     app.get('/user/profile', (req, res) => user_route.getProfile(req, res)); // lam duoc thi tot
     app.get('/user/notify', (req, res) => user_route.getNotify(req, res)); // lam duoc thi tot
 
-    app.post('/group/createGroup', jsonParser, (req, res) => groupadmin_route.createGroup(req, res)); //DONE
-    app.patch('/group/updateGroupInfo', jsonParser, (req, res) => groupadmin_route.updateGroupInfo(req, res)); //update cac thong tin group
-    app.patch('/group/updateGroupMembers', jsonParser, (req, res) => groupadmin_route.updateGroupMembers(req, res)); // them, sua, xoa member sau do gui lai danh sach member vao api nay
+    app.get('/group/', jsonParser, (req, res) => groupadmin_route.getGroupsByUserID(req, res)); //DONE
+    app.post('/group/', jsonParser, (req, res) => groupadmin_route.createGroup(req, res)); //DONE
+    app.put('/group/updateGroupInfo', jsonParser, (req, res) => groupadmin_route.updateGroupInfo(req, res)); //update cac thong tin group
+    app.put('/group/updateGroupMembers', jsonParser, (req, res) => groupadmin_route.updateGroupMembers(req, res)); // them, sua, xoa member sau do gui lai danh sach member vao api nay
     app.get('/group/members_time', (req, res) => groupadmin_route.getMembersTime(req, res)); //lay du lieu thong ke de show graph
     app.post('/group/createMeeting', jsonParser, (req, res) => groupadmin_route.createMeeting(req, res)); // tao meeting moi ( sau khi da xem thong ke)
     app.post('/group/midPoint', jsonParser, (req, res) => midPoint_route.MeatUp(req, res)); //lại send ở đây?
