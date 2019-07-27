46bf9708b00740727de94b6654ff28ea43a91b60
linhtq <tranquanglinh.pt@gmail.com>
2019-07-27 11:56:03 +0700

    sửa lỗi loạn x,y :( trong timemodel

--- Model/timemodel.js
+++ Model/timemodel.js
@@ -4,20 +4,21 @@ var result
 // {
 //   from: Date() bắt đầu từ khi nào,
 //   to:  Date() kết thúc khi nào,
-//   freeMember[]: Các thành viên có thể tham dự,
+//   freeMembers[]: Các thành viên có thể tham dự,
 // }
 result=[];
 
 
 // đầu vào của hàm dưới đây là một khoảng thời có cấu trúc như sau:
 // {
-//   username:định danh người dùng,
+//   name:định danh người dùng,
 //   from:Date() rảnh từ khi nào,
 //   to: Date() hết rảnh khi nào
 // }
 function addFreeTime(freeTimeObject){
-  var x1= freeTimeObject.from;
-  var x2= freeTimeObject.to;
+  let x1= freeTimeObject.from;
+  let x2= freeTimeObject.to;
+  let name = freeTimeObject.name;
     //duyệt tất cả các khung thời gian đã được thêm vào trước đó
     result.forEach(timeObj => {
         let y1= timeObj.from;
@@ -25,17 +26,17 @@ function addFreeTime(freeTimeObject){
         //xét từng trường hợp 1
         if(x1<=y1 && x2>=y2){
           // thời gian thêm vào chứa ít nhất 1 khoảng thời gian đã thêm vào trước đó
-          // new: |--------|
-          // old:   |-----|
+          // new(x): |--------|
+          // old(y):   |-----|
           // Todo: lấy khoảng thời gian bị trùng ra khỏi mảng result, tạo thành 3 khung thời gian mới,
           // đoạn trùng ở giữa thì cập nhập số người có thể tham dự rồi add trả vào mảng kết quả
           // 2 đoạn thừa ra 2 bên đầu bản chất sẽ là 2 bài toán mới với cách làm tương tự
 
         }
-        else if(x1<=y1 && x2>=y2){
+        else if(x1>=y1 && x2<=y2){
           // thời gian thêm vào nằm hoàn toàn trong 1 khoảng thời gian đã thêm vào trước đó
-          // new:   |--------|
-          // old: |-----------|
+          // new(x):   |--------|
+          // old(y): |-----------|
           //Todo: lấy khoảng thời gian bị trùng ra khỏi mảng result, tạo thành 3 khung thời gian mới,
           // cập nhập số người có thể tham dự ở đoạn giữa rồi add cả 3 đoạn vào mảng kết quả
 
@@ -44,15 +45,26 @@ function addFreeTime(freeTimeObject){
         }
 
         //2 trường hợp dưới đây làm tương tự theo logic trên
-        else if(x1<=y1 ** x2<=y2){
-          // new:   |--------|
-          // old:       |-------|
+        else if(x1<=y1 && x2<=y2){
+          // new(x):   |--------|
+          // old(y):       |-------|
 
         }
         else if(x1>=y1 && x2>=y2){
-          // new:   |--------|
-          // old: |-------|
+          // new(x):   |--------|
+          // old(y): |-------|
 
+        }else{
+          // new(x):             |--------|
+          // old(y): |-------|
+          // hoặc ngược lại
+          let newTimeObj={
+            from:x1,
+            to:x2,
+            freeMembers:[name]
+          }
+          result.push(newTimeObj);
         }
     });
 }
+
